0235912ac3c81d5222c0c896fc11a8b1d22a20ec
rocco-p <72693381+rocco-p@users.noreply.github.com>
2026-09-14 22:35:45 +0200

    add web

---
+++ web/scripts/bgaudio.js
@@ -0,0 +1,50 @@
+let isPlaying = false;
+
+function initAudio(src) {
+	try {
+		var sound      = document.createElement('audio');
+		sound.id       = 'audio-player';
+		sound.src      = src;
+		sound.type     = 'audio/mpeg';
+		sound.loop	   = true;
+		document.getElementById('root').appendChild(sound);
+	} catch(error) {
+		console.error('initAudio : ', error);
+	}
+}
+
+function playAudio() {
+	try {
+		document.getElementById('audio-player').play();
+		isPlaying = true;
+	} catch(error) {
+		console.error('playAudio : ', error);
+	}
+}
+
+function pauseAudio() {
+	try {
+		document.getElementById('audio-player').pause();
+	} catch(error) {
+		console.error('pauseAudio : ', error);
+	}
+}
+
+function resumeAudio() {
+	try {
+		document.getElementById('audio-player').play();
+		isPlaying = true;
+	} catch(error) {
+		console.error('resumeAudio : ', error);
+	}
+}
+
+function stopAudio() {
+	try {
+		document.getElementById('audio-player').pause();
+		document.getElementById('audio-player').currentTime =0;
+		isPlaying = false;
+	} catch(error) {
+		console.error('stopAudio : ', error);
+	}
+}
